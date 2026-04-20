e28184c71ad0ab1c71bb966adfdccaac8e3c32ed
benja742 <b3njaminandres@gmail.com>
2026-04-20 09:40:12 -0400

    concatenaciones

--- Mas ejercicios.py
+++ Mas ejercicios.py
@@ -1,12 +1,19 @@
 
-entrenador="Dawn"
+entrenador="Barry"
 
 pkm1=6
 
 entrenador2="Lance"
 pkm2=7
 
-if pkm2<=6:
-    print("Puede acceder a la liga pokemon")
-else:
-    print("Tiene mas pokemons de lo necesario")
+# if pkm2<=6:
+#     print("Puede acceder a la liga pokemon")
+# else:
+#     print("Tiene mas pokemons de lo necesario")
+
+# print(len(entrenador))
+
+print("El entrenador", entrenador, "tiene", pkm1, "pokemons")
+print("El entrenador", entrenador2, "tiene", pkm2, "pokemons")
+print(f"El entrenador {entrenador2} \ntiene {pkm2} pokemons")
+print("El entrenador "+ entrenador2+ " tiene 7 pokemons")
